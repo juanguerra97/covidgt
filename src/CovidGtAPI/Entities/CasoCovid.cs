4868387c60113ac8e83f6c0469741e75e2ed3b1f
Juan Guerra <juanguerra97@protonmail.com>
2020-10-01 10:42:57 -0600

    agregue campo Id

--- src/CovidGtAPI/Entities/CasoCovid.cs
+++ src/CovidGtAPI/Entities/CasoCovid.cs
@@ -4,6 +4,7 @@ namespace CovidGtAPI.Entities
 {
     public class CasoCovid
     {
+        public long Id { get; set; }
         public Localizacion Localizacion { get; set; }
         public DateTime FechaRegistro { get; set; }
         public int Edad { get; set; }
